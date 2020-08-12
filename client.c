8b3b6f4487cc36b4a645e1836dd6384720234974
Carlos Carral C <carloscarral13@gmail.com>
2020-08-11 23:28:31 -0500

    First round of corrections

--- client.c
+++ client.c
@@ -1,14 +1,15 @@
 /**
  * File              : client.c
  * Author            : Carlos Carral <carloscarral13@gmail.com>
- * Date              : 10.08.2020
- * Last Modified Date: 10.08.2020
+ * Date              : 10/08/2020
+ * Last Modified Date: 11/08/2020
  *
  * Simple socket client file
  *
  */
 
 #include "error.h"
+#include <arpa/inet.h>
 #include <netdb.h>
 #include <netinet/in.h>
 #include <stdio.h>
@@ -27,32 +28,39 @@ int main(int argc, char **argv)
   struct hostent *server;
   char buffer[256];
 
-  if (argc < 3) {
+  if (argc < 2) {
     fprintf(stderr, "usage: %s hostname port", argv[0]);
     exit(EXIT_FAILURE);
   }
 
-  portno = atoi(argv[2]);
+  printf("Bienvenido\n");
+
+  portno = atoi(argv[1]);
   socketfd = socket(AF_INET, SOCK_STREAM, 0);
+  printf("Requested socket\n");
 
   if (socketfd < 0)
     error("ERROR opening socket\n");
 
-  server = gethostbyname(argv[1]);
+  /* server = gethostbyname(argv[1]); */
+  /* printf("Got a server with name: %s", server->h_name); */
 
-  if (server == NULL) {
-    fprintf(stderr, "ERROR: No such host");
-    exit(EXIT_FAILURE);
-  }
+  /* if (server == NULL) { */
+  /* fprintf(stderr, "ERROR: No such host"); */
+  /* exit(EXIT_FAILURE); */
+  /* } */
 
   CLEAR(serv_addr);
 
   serv_addr.sin_family = AF_INET;
-
-  bcopy((char *)&server->h_addr, (char *)&serv_addr.sin_addr.s_addr,
-        server->h_length);
-
   serv_addr.sin_port = htons(portno);
+
+  if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) < 0) {
+    error("Invalid address\n");
+  }
+
+  /* bcopy((char *)&server->h_addr, (char *)&serv_addr.sin_addr.s_addr, */
+  /* server->h_length); */
 
   if (connect(socketfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
     error("ERROR connecting");
@@ -61,14 +69,14 @@ int main(int argc, char **argv)
   CLEAR(buffer);
   fgets(buffer, 255, stdin);
 
-  n = write(socketfd, buffer, strlen(buffer));
+  n = send(socketfd, buffer, strlen(buffer), 0);
 
   if (n < 0)
     error("ERROR Writing to socket");
 
   CLEAR(buffer);
 
-  n = read(socketfd, buffer, 255);
+  n = read(socketfd, buffer, 256);
 
   if (n < 0)
     error("ERROR Reading from socket");
--- server.c
+++ server.c
@@ -2,12 +2,14 @@
  * File              : server.c
  * Author            : Carlos Carral <carloscarral13@gmail.com>
  * Date              : 10/08/2020
- * Last Modified Date: 10.08.2020
+ * Last Modified Date: 11/08/2020
  *
  * Simple socket server file
+ *
  */
 
 #include "error.h"
+#include <asm-generic/socket.h>
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -28,12 +30,16 @@ int main(int argc, char **argv)
   struct sockaddr_in serv_addr, cli_addr;
 
   int n;
+  int opt = 1;
 
   if (argc < 2) {
     fprintf(stderr, "ERROR, no port provided\n");
     exit(1);
   }
 
+  printf("Bienvenido\n");
+
+  // Creating socket file descriptor
   socketfd = socket(AF_INET, SOCK_STREAM, 0);
 
   if (socketfd < 0)
@@ -42,36 +48,47 @@ int main(int argc, char **argv)
   CLEAR(serv_addr);
 
   portno = atoi(argv[1]);
+  printf("Listening on port %d", portno);
+
+  if (setsockopt(socketfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt,
+                 sizeof(opt)))
+    error("setsockopt");
 
   serv_addr.sin_family = AF_INET;
   serv_addr.sin_port = htons(portno);
   serv_addr.sin_addr.s_addr = INADDR_ANY;
 
-  if (bind(socketfd, (struct sockaddr *)&cli_addr, sizeof(serv_addr)) < 0)
+  if (bind(socketfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
     error("ERROR on binding");
 
-  listen(socketfd, 5);
+  if (listen(socketfd, 5) < 0)
+    error("listen");
 
   clilen = sizeof(cli_addr);
 
-  newsockfd = accept(socketfd, (struct sockaddr *)&cli_addr, &clilen);
+  newsockfd =
+          accept(socketfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
 
   if (newsockfd < 0)
     error("ERROR on accept");
 
   CLEAR(buffer);
 
-  n = read(newsockfd, buffer, 255);
+  n = read(newsockfd, buffer, 256);
 
   if (n < -1)
     error("ERROR Reading from socket");
 
   printf("Message: %s\n", buffer);
 
-  n = write(newsockfd, "Message received", 18);
+  char *return_msg = "Message received";
+
+  n = send(newsockfd, return_msg, strlen(return_msg), 0);
 
   if (n < 0)
     error("ERROR writing to socket");
+
+  printf("Response sent");
 
   return 0;
 }
